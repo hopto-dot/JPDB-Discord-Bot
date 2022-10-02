02ad8b60f685da0f4b3b232fa634ec7b761dd454
JawGBoi <66906618+hopto-dot@users.noreply.github.com>
2022-10-02 08:17:25 +0100

    Added !helprequest

--- JPDB Bot/Commands/Rules.cs
+++ JPDB Bot/Commands/Rules.cs
@@ -14,11 +14,26 @@ namespace JPDB_Bot.Commands
     {
         [Command("rule6")]
         [Cooldown(1, 20, CooldownBucketType.User)]
-        [Hidden]
-        [Description("Shows rule 6 for people that haven't read the rules :harold:")]
+        [Description("Shows rule 6 for people that haven't read the rules")]
         public async Task rule6(CommandContext ctx)
         {
             await ctx.Channel.SendMessageAsync("If you've just joined the server do not ask us to translate something for you and then leave. We're not a free translation service. We're always happy to help people in our community, but if you only want to use us then don't bother.").ConfigureAwait(false);
+        }
+
+        [Command("rule34")]
+        [Cooldown(1, 30, CooldownBucketType.User)]
+        [Hidden]
+        public async Task rule34(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Rule 34 doesn't exist on this server, seriously.").ConfigureAwait(false);
+        }
+
+        [Command("helprequest")]
+        [Aliases("context", "requesthelp")]
+        [Cooldown(1, 30, CooldownBucketType.User)]
+        public async Task helprequest(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("When requesting help on a specific usage, please include, as applicable, what the source of the usage is, as well as either a direct copy of the text or a screenshot if not copyable of the usage and surrounding context. Japanese is a heavily context-based language and you will likely not get good answers if the responder has to assume key details.").ConfigureAwait(false);
         }
     }
 }
